df3468eb90a76993311d459f72a768f0dd5a53c8
shanshe-7 <shanshe1993@gmail.com>
2024-10-15 02:12:24 +0400

    feat: add some stiling

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -4,9 +4,20 @@
 </script>
 
 <div class="max-w-5xl mx-auto">
-  <div class="flex justify-between pl-5 pr-5 border-b border-surface-50">
-    <Button variant="link">Bookmarks</Button>
-    <Button variant="link">Full Urls</Button>
+  <div
+    class="flex justify-between pl-5 pr-5 border-b border-surface-50 pt-4 pb-3 mb-2"
+  >
+    <a
+      class="text-primary underline-offset-4 hover:underline visited:text-purple-600"
+      variant="link"
+      href="/">ტურნირები</a
+    >
+
+    <a
+      class="text-primary underline-offset-4 hover:underline"
+      variant="link"
+      href="/questions">შესვლა</a
+    >
   </div>
-  <slot></slot>
+  <slot />
 </div>

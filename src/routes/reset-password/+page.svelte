b2101bb9d79c49245806d24aab268ddc7670d0af
shanshe-7 <shanshe1993@gmail.com>
2024-10-21 17:15:45 +0400

    feat: password reset

--- src/routes/reset-password/+page.svelte
+++ src/routes/reset-password/+page.svelte
@@ -1,4 +1,5 @@
 <script lang="js">
+  import { page } from "$app/stores";
   import * as Form from "$lib/components/ui/form";
   import { Input } from "$lib/components/ui/input";
   import { superForm } from "sveltekit-superforms";
@@ -9,10 +10,10 @@
   import { z } from "zod";
 
   const formSchema = z.object({
-    email: z.string().email("მეილის არასწორი მისამართი"), // Required valid email
     password: z
       .string()
       .min(6, "პასვორდი უნდა შედგებოდეს მინიმუმ 6 სიმბოლოსგან"),
+    repeat: z.string().min(6, "პასვორდი უნდა შედგებოდეს მინიმუმ 6 სიმბოლოსგან"),
   });
 
   export let data = {};
@@ -31,12 +32,23 @@
     if (!validationResult.success) {
       errors = validationResult.error.formErrors.fieldErrors;
     } else {
-      errors = {};
+      const token = $page.url.searchParams.get("token");
+
+      if ($formData.password !== $formData.repeat) {
+        errors["repeat"] = "პასვორდი არ ემთხვევა";
+        return;
+      }
+
       try {
-        fetch("http://localhost:8080/api/public/login", {
+        errors = {};
+
+        fetch("http://localhost:8080/api/public/reset-password", {
           method: "POST",
           ["Content-type"]: "application/json",
-          body: JSON.stringify($formData),
+          body: JSON.stringify({
+            password: $formData.password,
+            token,
+          }),
         });
       } catch (error) {
         console.log("error", error);
@@ -45,34 +57,42 @@
   }
 </script>
 
-<div>
-  <Link>დაგავიწყდა პაროლი?</Link>
+<div class="flex justify-center items-center flex-col gap-8 p-5 w-full">
+  <form on:submit={handleSubmit} class="w-full md:w-2/4">
+    <Form.Field {form} name="password">
+      <Form.Control let:attrs>
+        <Form.Label>პასვორდი</Form.Label>
+        <Input
+          required
+          type="password"
+          {...attrs}
+          bind:value={$formData.password}
+        />
+      </Form.Control>
+      <Form.FieldErrors
+        ><p class="p-0 h-5">
+          {errors.hasOwnProperty("password") ? errors["password"] : ""}
+        </p></Form.FieldErrors
+      >
+    </Form.Field>
+
+    <Form.Field {form} name="repeat">
+      <Form.Control let:attrs>
+        <Form.Label>გაიმეორეთ პასვორდი</Form.Label>
+        <Input
+          type="password"
+          required
+          {...attrs}
+          bind:value={$formData.repeat}
+        />
+      </Form.Control>
+      <Form.FieldErrors
+        ><p class="p-0 h-5">
+          {errors.hasOwnProperty("repeat") ? errors["repeat"] : ""}
+        </p></Form.FieldErrors
+      >
+    </Form.Field>
+
+    <Form.Button class="mt-6" type="submit">გაგზავნა</Form.Button>
+  </form>
 </div>
-
-<form on:submit={handleSubmit} class="w-full md:w-2/4">
-  <Form.Field {form} name="email">
-    <Form.Control let:attrs>
-      <Form.Label>მეილი</Form.Label>
-      <Input required type="email" {...attrs} bind:value={$formData.email} />
-    </Form.Control>
-    <Form.FieldErrors
-      ><p class="p-0 h-5">
-        {errors.hasOwnProperty("email") ? errors["email"] : ""}
-      </p></Form.FieldErrors
-    >
-  </Form.Field>
-
-  <Form.Field {form} name="password">
-    <Form.Control let:attrs>
-      <Form.Label>პასვორდი</Form.Label>
-      <Input required {...attrs} bind:value={$formData.password} />
-    </Form.Control>
-    <Form.FieldErrors
-      ><p class="p-0 h-5">
-        {errors.hasOwnProperty("password") ? errors["password"] : ""}
-      </p></Form.FieldErrors
-    >
-  </Form.Field>
-
-  <Form.Button class="mt-6" type="submit">შესვლა</Form.Button>
-</form>
